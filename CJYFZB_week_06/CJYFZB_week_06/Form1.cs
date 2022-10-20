b88fa5ce025a5919142e21b7acd055039eb826bc
LucaCorvinus <luca.szabo3@stud.uni-corvinus.hu>
2022-10-20 11:20:09 +0200

    IRF week_06 3. feladat

--- CJYFZB_week_06/CJYFZB_week_06/Form1.cs
+++ CJYFZB_week_06/CJYFZB_week_06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CJYFZB_week_06.MNBServiceReference;
 
 namespace CJYFZB_week_06
 {
@@ -15,6 +16,24 @@ namespace CJYFZB_week_06
         public Form1()
         {
             InitializeComponent();
+            WebHivas();
+        }
+
+        private void WebHivas()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
         }
     }
 }
