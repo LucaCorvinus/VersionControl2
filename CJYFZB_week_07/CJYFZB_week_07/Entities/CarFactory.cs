819f864371a63a0de94f4df9def161995e3f9fff
LucaCorvinus <luca.szabo3@stud.uni-corvinus.hu>
2022-10-29 21:19:38 +0200

    IRF week_07 9. feladat

--- CJYFZB_week_07/CJYFZB_week_07/Entities/CarFactory.cs
+++ CJYFZB_week_07/CJYFZB_week_07/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace CJYFZB_week_07.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
-       public Toy CreateNew()
+        public Toy CreateNew()
         {
-            return new Ball(); 
+            return new Car();
         }
     }
 }
