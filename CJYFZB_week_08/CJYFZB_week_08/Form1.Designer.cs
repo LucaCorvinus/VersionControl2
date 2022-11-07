2a665cad8b9309cd9de1633d2268caa7db96675e
LucaCorvinus <luca.szabo3@stud.uni-corvinus.hu>
2022-11-07 14:32:36 +0100

    IRF week_08 5. feladat

--- CJYFZB_week_08/CJYFZB_week_08/Form1.Designer.cs
+++ CJYFZB_week_08/CJYFZB_week_08/Form1.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.dgw_Ticks = new System.Windows.Forms.DataGridView();
+            this.dgw_Portfolio = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dgw_Ticks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_Portfolio)).BeginInit();
             this.SuspendLayout();
             // 
             // dgw_Ticks
@@ -39,18 +41,30 @@
             this.dgw_Ticks.Name = "dgw_Ticks";
             this.dgw_Ticks.RowHeadersWidth = 62;
             this.dgw_Ticks.RowTemplate.Height = 28;
-            this.dgw_Ticks.Size = new System.Drawing.Size(577, 426);
+            this.dgw_Ticks.Size = new System.Drawing.Size(447, 426);
             this.dgw_Ticks.TabIndex = 0;
+            // 
+            // dgw_Portfolio
+            // 
+            this.dgw_Portfolio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_Portfolio.Location = new System.Drawing.Point(466, 13);
+            this.dgw_Portfolio.Name = "dgw_Portfolio";
+            this.dgw_Portfolio.RowHeadersWidth = 62;
+            this.dgw_Portfolio.RowTemplate.Height = 28;
+            this.dgw_Portfolio.Size = new System.Drawing.Size(240, 425);
+            this.dgw_Portfolio.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgw_Portfolio);
             this.Controls.Add(this.dgw_Ticks);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgw_Ticks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_Portfolio)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgw_Ticks;
+        private System.Windows.Forms.DataGridView dgw_Portfolio;
     }
 }
 
--- CJYFZB_week_08/CJYFZB_week_08/Form1.cs
+++ CJYFZB_week_08/CJYFZB_week_08/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CJYFZB_week_08.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,11 +15,23 @@ namespace CJYFZB_week_08
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dgw_Ticks.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dgw_Portfolio.DataSource = Portfolio;
         }
     }
 }
