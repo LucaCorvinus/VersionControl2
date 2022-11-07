7c8988026994f8f03116be4e584d83fc80206311
LucaCorvinus <luca.szabo3@stud.uni-corvinus.hu>
2022-11-07 14:25:12 +0100

    IRF week_08 4. feladat

--- CJYFZB_week_08/CJYFZB_week_08/Form1.Designer.cs
+++ CJYFZB_week_08/CJYFZB_week_08/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgw_Ticks = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_Ticks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgw_Ticks
+            // 
+            this.dgw_Ticks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_Ticks.Location = new System.Drawing.Point(12, 12);
+            this.dgw_Ticks.Name = "dgw_Ticks";
+            this.dgw_Ticks.RowHeadersWidth = 62;
+            this.dgw_Ticks.RowTemplate.Height = 28;
+            this.dgw_Ticks.Size = new System.Drawing.Size(577, 426);
+            this.dgw_Ticks.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgw_Ticks);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_Ticks)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgw_Ticks;
     }
 }
 
