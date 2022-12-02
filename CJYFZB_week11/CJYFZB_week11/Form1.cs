6efbccf78226529b3a6541fbbd88bdc3b2cf0db3
LucaCorvinus <luca.szabo3@stud.uni-corvinus.hu>
2022-12-02 11:08:42 +0100

    IRF week_11 5. feladat

--- CJYFZB_week11/CJYFZB_week11/Form1.cs
+++ CJYFZB_week11/CJYFZB_week11/Form1.cs
@@ -27,7 +27,25 @@ namespace CJYFZB_week11
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
 
-            //dataGridView1.DataSource = DeathProbabilities;  
+            //dataGridView1.DataSource = DeathProbabilities;
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
